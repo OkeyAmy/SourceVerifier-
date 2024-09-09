61dfd64d4e18e8a994952eb100847ffee73f48a0
Peter Jung <peter@jung.ninja>
2024-09-09 11:51:28 +0200

    use pmat logger

--- factcheck/utils/logger.py
+++ factcheck/utils/logger.py
@@ -2,6 +2,7 @@ import os
 import logging
 from flask import g
 from logging.handlers import TimedRotatingFileHandler
+from prediction_market_agent_tooling.loggers import logger
 
 
 class CustomLogger:
@@ -13,26 +14,7 @@ class CustomLogger:
             loglevel (_type_, optional): the log level. Defaults to logging.INFO.
         """
         # Create a custom logger
-        self.logger = logging.getLogger("FactCheck")
-        self.logger.setLevel(loglevel)
-        # Create a handler for writing to the log file
-        if not os.path.exists("./log"):
-            # If the directory does not exist, create it
-            os.makedirs("./log")
-        env = os.environ.get("env", "dev")
-        fh = TimedRotatingFileHandler(filename="./log/factcheck_{}.log".format(env), when="D")
-        fh.setLevel(loglevel)
-        if not self.logger.handlers:
-            # Create another handler for output to the console
-            ch = logging.StreamHandler()
-            ch.setLevel(loglevel)
-            # Define the output format of the handler
-            formatter = logging.Formatter("[%(levelname)s]%(asctime)s %(filename)s:%(lineno)d: %(message)s")
-            fh.setFormatter(formatter)
-            ch.setFormatter(formatter)
-            # Add handler to logger
-            self.logger.addHandler(fh)
-            self.logger.addHandler(ch)
+        self.logger = logger
 
     def getlog(self):
         return self.logger
